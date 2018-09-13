498e2a1fc455dad61703463b4b1108e4cd0423dc
Katy Osmond <katy2@katys-MacBook-Pro.local>
2018-09-13 22:07:38 +1000

    portfolio work

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -4,34 +4,63 @@ import Logo from '../../images/logo.png';
 export default class App extends Component {
     render() {
         return (
-            <header className="nav">
-                <div className="container">
-                    <div className="nav-items">
-                        <div className="nav-item toggler">menu</div>
-                        <div className="nav-item logo">
-                            <img src={Logo} alt="" />
-                        </div>
-                        <div className="nav-item links">
-                            <li>
-                                <a className="active" href="#portfolio">
-                                    Portfolio
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#about">About</a>
-                            </li>
-                            <li>
-                                <a href="#contact">Contact</a>
-                            </li>
-                            <li>
-                                <a href="#instagram">
-                                    <i className="fab fa-instagram" />
-                                </a>
-                            </li>
+            <div className="page-wrap">
+                <header className="nav">
+                    <div className="container">
+                        <div className="nav-items">
+                            <div className="nav-item toggler">menu</div>
+                            <div className="nav-item logo">
+                                <img src={Logo} alt="" />
+                            </div>
+                            <div className="nav-item links">
+                                <li>
+                                    <a
+                                        href="#portfolio"
+                                        className="active link-text"
+                                    >
+                                        Portfolio
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#about" className="link-text">
+                                        About
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#contact" className="link-text">
+                                        Contact
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#instagram" className="link-text">
+                                        <i className="fab fa-instagram" />
+                                    </a>
+                                </li>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </header>
+                </header>
+                <main className="portfolio-grid">
+                    <div className="container">
+                        <div className="grid-items">
+                            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((_, i) => (
+                                <div className="grid-item">
+                                    <img
+                                        src={`https://picsum.photos/500/500/?image=${i +
+                                            106}`}
+                                        alt=""
+                                    />
+                                    <p>
+                                        <a href="#" className="link-text">
+                                            Title of work
+                                        </a>
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </main>
+            </div>
         );
     }
 }
