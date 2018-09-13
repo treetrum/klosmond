9eddc9265d5b59f69a142b3ade43fe2414c528a9
Katy Osmond <katy2@katys-MacBook-Pro.local>
2018-09-13 21:54:16 +1000

    nav work

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -6,27 +6,28 @@ export default class App extends Component {
         return (
             <header className="nav">
                 <div className="container">
-                    <div className="inner">
-                        <div className="nav-items">
-                            <div className="nav-item logo">
-                                <img src={Logo} alt="" />
-                            </div>
-                            <div className="nav-item links">
-                                <li>
-                                    <a href="#portfolio">Portfolio</a>
-                                </li>
-                                <li>
-                                    <a href="#about">About</a>
-                                </li>
-                                <li>
-                                    <a href="#contact">Contact</a>
-                                </li>
-                                <li>
-                                    <a href="#instagram">
-                                        <i className="fab fa-instagram" />
-                                    </a>
-                                </li>
-                            </div>
+                    <div className="nav-items">
+                        <div className="nav-item toggler">menu</div>
+                        <div className="nav-item logo">
+                            <img src={Logo} alt="" />
+                        </div>
+                        <div className="nav-item links">
+                            <li>
+                                <a className="active" href="#portfolio">
+                                    Portfolio
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#about">About</a>
+                            </li>
+                            <li>
+                                <a href="#contact">Contact</a>
+                            </li>
+                            <li>
+                                <a href="#instagram">
+                                    <i className="fab fa-instagram" />
+                                </a>
+                            </li>
                         </div>
                     </div>
                 </div>
