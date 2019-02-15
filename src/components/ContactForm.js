e2a022c353cfdc4987a433a83af8b7fd50884517
Sam Davis <sam@Sams-Mac-mini.local>
2019-02-16 10:58:57 +1100

    make form better

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,85 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SDTextInput from '../components/SDTextInput';
 
 const ContactForm = () => {
-	const [submitAttempted, setSubmitAttempted] = useState(false);
-	const [inputValues, updateInputValues] = useState({ name: '', email: '', message: '' });
-	const [inputErrors, updateInputErrors] = useState({ name: '', email: '', message: '' });
-
-	const onInputChange = e => {
-		const newValues = {
-			...inputValues,
-			[e.target.name]: e.target.value
-		};
-		updateInputValues(newValues);
-		if (submitAttempted) {
-			validateSingleInput(e.target.name, e.target.value);
-		}
+	const inputs = {
+		name: useInput(''),
+		email: useInput('', validateEmail),
+		message: useInput('')
 	};
 
 	const onFormSubmit = e => {
-		const isValid = validateAllInputs();
-		if (!isValid) {
+		let formValid = true;
+		Object.keys(inputs)
+			.map(k => inputs[k])
+			.map(input => {
+				const result = input.validate();
+				formValid = formValid ? result : false;
+			});
+		if (!formValid) {
 			e.preventDefault();
-			setSubmitAttempted(true);
 		}
 	};
-
-	const validateAllInputs = () => {
-		const newErrors = { ...inputErrors };
-		Object.keys(inputValues).forEach(key => {
-			const value = inputValues[key];
-			newErrors[key] = validationFunction(key, value);
-		});
-		updateInputErrors(newErrors);
-		let isValid = true;
-		Object.keys(newErrors).forEach(key => {
-			const value = newErrors[key];
-			isValid = !value;
-		});
-		return isValid;
-	};
-
-	const validateSingleInput = (key, value) => {
-		const newErrors = { ...inputErrors, [key]: validationFunction(key, value) };
-		updateInputErrors(newErrors);
-	};
-
-	const validationFunction = (key, value) => {
-		let error = false;
-		switch (key) {
-			case 'name':
-				if (value.length === 0) {
-					error = 'Name is required';
-					break;
-				}
-				break;
-			case 'email':
-				if (value.length === 0) {
-					error = 'Email is required';
-					break;
-				}
-				if (!validateEmail(value)) {
-					error = 'Not a valid email address';
-					break;
-				}
-				break;
-			case 'message':
-				if (value.length === 0) {
-					error = 'Message is required';
-					break;
-				}
-				break;
-			default:
-				break;
-		}
-		return error;
-	};
-
-	function validateEmail(email) {
-		var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return re.test(String(email).toLowerCase());
-	}
 
 	return (
 		<form className="sd-form" name="contact" method="POST" data-netlify="true" onSubmit={onFormSubmit}>
@@ -88,25 +28,25 @@ const ContactForm = () => {
 				label="Name"
 				name="name"
 				placeholder="Jane Smith"
-				error={inputErrors.name}
-				value={inputValues.name}
-				onChange={onInputChange}
+				value={inputs.name.value}
+				onChange={inputs.name.onChange}
+				error={inputs.name.error}
 			/>
 			<SDTextInput
 				label="Email"
 				name="email"
 				placeholder="jane.smith@example.com"
-				value={inputValues.email}
-				onChange={onInputChange}
-				error={inputErrors.email}
+				value={inputs.email.value}
+				onChange={inputs.email.onChange}
+				error={inputs.email.error}
 			/>
 			<SDTextInput
 				label="Message"
 				name="message"
-				value={inputValues.message}
-				onChange={onInputChange}
 				type="textarea"
-				error={inputErrors.message}
+				value={inputs.message.value}
+				onChange={inputs.message.onChange}
+				error={inputs.message.error}
 			/>
 			<div className="sd-input">
 				<button className="sd-input__submit" type="submit">
@@ -115,6 +55,50 @@ const ContactForm = () => {
 			</div>
 		</form>
 	);
+};
+
+const useInput = (defaultValue, customValidation = false) => {
+	const [value, setValue] = useState(defaultValue);
+	const [error, setError] = useState('');
+
+	const onChange = e => {
+		setValue(e.target.value);
+	};
+
+	const validate = () => {
+		const status = value.trim() ? true : false;
+		setError(status ? '' : 'This field is required');
+		if (status && customValidation) {
+			let a = customValidation(value);
+			if (!a.valid) {
+				setError(a.message);
+			}
+		}
+		return status;
+	};
+
+	// Validates constantly if there's an error
+	useEffect(() => {
+		if (error) {
+			validate();
+		}
+	}, [value]);
+
+	return {
+		value,
+		error,
+		onChange,
+		validate
+	};
+};
+
+const validateEmail = email => {
+	var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	const result = re.test(String(email).toLowerCase());
+	return {
+		valid: result,
+		message: result ? '' : 'Not a valid email'
+	};
 };
 
 export default ContactForm;
